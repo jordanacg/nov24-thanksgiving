2650f86dfd66f7e6c39f0282927f9a593cc39927
Jordan Chong-Gayle <jordanchong-gayle@Jordans-MacBook-Air.local>
2022-11-24 18:21:50 -0500

    hello

---
+++ script.js
@@ -0,0 +1,9 @@
+function openBtn() {
+  document.querySelector(".sidenav").style.width = "250px";
+  document.querySelector(".main").style.marginLeft = "250px";
+}
+
+function closeBtn() {
+  document.querySelector(".sidenav").style.width = "0";
+  document.querySelector(".main").style.marginLeft = "0";
+}
